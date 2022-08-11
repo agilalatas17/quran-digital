2eab2650646cfde6193666770f133f607bf2b73b
Agil_Alatas <agilalatas17@gmail.com>
2022-08-11 18:51:23 +0700

    delete Footer Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import CardSurah from "../components/CardSurah";
 import HeroSection from "../components/hero-section/HeroSection";
-import Footer from "../components/footer/Footer";
 
 function Home() {
   return (
     <div>
       <HeroSection />
       <CardSurah />
-      <Footer />
     </div>
   );
 }
